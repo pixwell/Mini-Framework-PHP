fef638e9890d7a84ea6ea33d949dba40a6bdb96d
Marcia Silva <contato.pix@gmail.com>
2019-03-06 18:04:41 -0300

    Criado boostrap.php para inicialização da aplicação

--- public/index.php
+++ public/index.php
@@ -1,21 +1,4 @@
 <?php 
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$router = new \Core\Router();
-
-// Add the routes
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
-    
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+require __DIR__ . '/../public/bootstrap.php';
