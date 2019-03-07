caabe795dca0184f697a92aebc4779d9ca4f3626
Marcia Silva <contato.pix@gmail.com>
2019-03-07 12:26:23 -0300

    Carregando o arquivo de definição de rotas e passando como parâmetro para a classe Router

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-
-$routes = new Core\Router();
-$routes->getUrl();
+//Variavel com o arquivo de rotas
+$routeFile = require_once __DIR__ . '/../App/routes.php';
+//Instanciando a classe Router e passando a tabela de rotas (Table Routing)
+$route = new Core\Router($routeFile);
