30e4cf02489e8077c8d5315f9c9844670e80fce8
Marcia Silva <contato.pix@gmail.com>
2019-03-07 12:29:06 -0300

    Inserido comentário explicativo

--- App/routes.php
+++ App/routes.php
@@ -1,4 +1,11 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Tabela de rotas (Routing table)
+|--------------------------------------------------------------------------|
+| Aqui serao definidas as rotas de sistema.
+|
+*/
 
 return [
     //[Rota, controller@ation]
