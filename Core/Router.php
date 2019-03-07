9e28bbb2ad876b010af44485ab747158fcd03d9d
Marcia Silva <contato.pix@gmail.com>
2019-03-07 12:25:41 -0300

    Método construct para trazer a tabela de rotas (table routing)

--- Core/Router.php
+++ Core/Router.php
@@ -4,6 +4,13 @@ namespace Core;
 
 class Router
 {
+    private $routes;
+    
+    public function __construct(array $routeParam)
+    {
+        $this->routes = $routeParam;
+    }
+    
     public function getUrl()
     {
         $url = filter_input(INPUT_SERVER, REQUEST_URI);
