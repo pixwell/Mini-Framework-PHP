d5a4c71b97210c696ce4ac86434f763d7d2c668c
Marcia Silva <contato.pix@gmail.com>
2019-03-07 11:57:18 -0300

    Refazendo a Classe para Roteamento

--- Core/Router.php
+++ Core/Router.php
@@ -4,58 +4,5 @@ namespace Core;
 
 class Router
 {
-    /**
-     * Associative array of routes (the routing table)
-     * @var array
-     */
-    private $routes = [];
     
-    /**
-     * Parameters from the matched route
-     * @var array
-     */
-    protected $params = [];
-
-    /**
-     * Add a route to the routing table
-     *
-     * @param string $route  The route URL
-     * @param array  $params Parameters (controller, action, etc.)
-     *
-     * @return void
-     */
-    public function add($route, $params)
-    {
-        $this->routes[$route] = $params;
-    }
-
-    /**
-     * Match the route to the routes in the routing table, setting the $params
-     * property if a route is found.
-     *
-     * @param string $url The route URL
-     *
-     * @return boolean  true if a match found, false otherwise
-     */
-    public function match($url)
-    {
-        foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
-                $this->params = $params;
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Get the currently matched parameters
-     *
-     * @return array
-     */
-    public function getParams()
-    {
-        return $this->params;
-    }
 }
