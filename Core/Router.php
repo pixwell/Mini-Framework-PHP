1fb10770b8c2d97c8e647fcd063ffe1fd8777377
Marcia Silva <contato.pix@gmail.com>
2019-03-07 12:07:07 -0300

    Método para trazer a URL (path) atual

--- Core/Router.php
+++ Core/Router.php
@@ -4,5 +4,9 @@ namespace Core;
 
 class Router
 {
-    
+    public function getUrl()
+    {
+        $url = filter_input(INPUT_SERVER, REQUEST_URI);
+        return parse_url($url, PHP_URL_PATH);
+    }
 }
