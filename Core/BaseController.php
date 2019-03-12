0472c4e128f79f6f9d1409458a83beb1bafaf23a
Marcia Silva <contato.pix@gmail.com>
2019-03-12 15:29:24 -0300

    Criado método para incluir o conteúdo do arquivo de view

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -19,5 +19,17 @@ abstract class BaseController
     protected function renderView($path)
     {
         $this->viewPath = $path;
+        $this->contentView();
+    }
+    
+    /**
+     * Traz o conteudo da view
+     */
+    protected function contentView() {
+        if( file_exists(__DIR__ . '/../App/Views/' . $this->viewPath . '.phtml') ){
+            require __DIR__ . '/../App/Views/' . $this->viewPath . '.phtml';
+        } else {
+            echo 'Erro: arquivo de View não existe.';
+        }
     }
 }
