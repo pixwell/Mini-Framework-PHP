264c97e6cad2ab731c2a6df5fc87cf1a685d3160
Marcia Silva <contato.pix@gmail.com>
2019-03-12 15:19:23 -0300

    Método para definir o caminho para importar a view

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -5,9 +5,19 @@ namespace Core;
 abstract class BaseController
 {
     protected $view;
+    private $viewPath;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->view = new \stdClass();
     }
-
+    
+    /**
+     * Define o caminho da view
+     * @param string $path
+     */
+    protected function renderView($path)
+    {
+        $this->viewPath = $path;
+    }
 }
