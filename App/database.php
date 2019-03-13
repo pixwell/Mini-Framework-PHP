b11101f863403ad34fa48602d2bcaf04dc67a9b0
Marcia Silva <contato.pix@gmail.com>
2019-03-13 15:09:16 -0300

    Correção nos atributos necessários

--- App/database.php
+++ App/database.php
@@ -8,9 +8,7 @@ return [
     'driver' => 'sqlite',
     
     'sqlite' => [
-        'host' => 'database.db',
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci'
+        'host' => 'database.db'
     ],
     'mysql' => [
         'host' => 'localhost',
