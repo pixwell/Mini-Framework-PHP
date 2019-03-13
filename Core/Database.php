bda72792cf5c52938f62d888fba4e16534ea84ee
Marcia Silva <contato.pix@gmail.com>
2019-03-13 15:22:21 -0300

    Programação da conexão para o driver SQLite

--- Core/Database.php
+++ Core/Database.php
@@ -3,10 +3,29 @@
 namespace Core;
 
 /**
- * Description of Database
+ * Manipulacao do banco de dados
  *
- * @author pix
+ * @author pixwell
  */
 class Database {
-    //put your code here
+            
+    public function getDatabase(){
+        $conf = include_once __DIR__ . '/../App/database.php';
+        if( $conf['driver'] == 'sqlite' ){
+            $sqliteDBFile = include_once __DIR__ . '/../Storage/database/' . $conf['sqlite']['host'];
+            $sqliteDNS = 'sqlite:' . $sqliteDBFile;
+            
+            //Tentar conectar
+            try{
+                $pdo = new \PDO($sqliteDNS);
+                //Tipo de erro
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //Retornar um objeto nas consultas
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                return $pdo;
+            }catch( \PDOException $e ){
+                echo $e->getMessage();
+            }
+        }
+    }
 }
