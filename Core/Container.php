3e5e0081bbff79418911625644dc8e03e8e6e7bb
Marcia Silva <contato.pix@gmail.com>
2019-03-13 13:56:51 -0300

    Criado método para chamar página de erro

--- Core/Container.php
+++ Core/Container.php
@@ -9,4 +9,12 @@ class Container
         $objController = 'App\\Controllers\\' . $controller;
         return new $objController;
     }
+    
+    public static function pageNotFound(){
+        if (file_exists(__DIR__ . '/../App/Views/404.phtml')) {
+            require __DIR__ . '/../App/Views/404.phtml';
+        } else {
+            echo 'Erro: arquivo de View 404 não existe.';
+        }
+    }
 }
