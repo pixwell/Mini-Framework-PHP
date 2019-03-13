db081522df75c7a7a7a93bd17b4a13a53dc3503c
Marcia Silva <contato.pix@gmail.com>
2019-03-13 16:55:28 -0300

    Método all() para trazer todos os registros de uma tabela

--- Core/BaseModel.php
+++ Core/BaseModel.php
@@ -12,4 +12,14 @@ abstract class BaseModel {
     {
         $this->pdoConn = $pdoObj;
     }
+    
+    public function all()
+    {
+        $query = 'SELECT * FROM ' . $this->table;
+        $stmt = $this->pdoConn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        $stmt->closeCursor();
+        return $result;
+    }
 }
